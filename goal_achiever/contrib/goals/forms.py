a3270e904441c126d0683d3286beae13d0174bc5
Procelius <milisent@protonmail.ch>
2020-02-12 00:38:12 +0200

    add task and restrictions forms to the goal list view and template, add hidden action inputs

--- goal_achiever/contrib/goals/forms.py
+++ goal_achiever/contrib/goals/forms.py
@@ -13,8 +13,34 @@ class GoalForm(forms.ModelForm):
             'restrictions',
         ]
         widgets = {
-            'name': forms.TextInput,
-            'description': forms.TextInput,
+            'name': forms.TextInput(attrs={'id': 'goal-name'}),
+            'description': forms.TextInput(attrs={'id': 'goal-description'}),
             'tasks': forms.CheckboxSelectMultiple,
             'restrictions': forms.CheckboxSelectMultiple,
+        }
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = models.Task
+        fields = [
+            'name',
+            'description'
+        ]
+        widgets = {
+            'name': forms.TextInput(attrs={'id': 'task-name'}),
+            'description': forms.TextInput(attrs={'id': 'task-description'}),
+        }
+
+
+class RestrictionForm(forms.ModelForm):
+    class Meta:
+        model = models.Restriction
+        fields = [
+            'name',
+            'description'
+        ]
+        widgets = {
+            'name': forms.TextInput(attrs={'id': 'restriction-name'}),
+            'description': forms.TextInput(attrs={'id': 'restriction-description'}),
         }
--- goal_achiever/contrib/goals/views.py
+++ goal_achiever/contrib/goals/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 
 from .models import Goal
-from .forms import GoalForm
+from .forms import GoalForm, TaskForm, RestrictionForm
 
 
 def goal_list(request):
     goals = Goal.objects.all()
+    goal_form = GoalForm()
+    task_form = TaskForm()
+    restriction_form = RestrictionForm()
     if request.POST:
-        goal_form = GoalForm(request.POST)
-        if goal_form.is_valid():
-            goal_form.save()
-    else:
-        goal_form = GoalForm()
+        action = request.POST["action"]
+        if action == "new_goal":
+            goal_form = GoalForm(request.POST)
+            if goal_form.is_valid():
+                goal_form.save()
+        elif action == "new_task":
+            task_form = TaskForm(request.POST)
+            if task_form.is_valid():
+                task_form.save()
+        elif action == "new_restriction":
+            restriction_form = RestrictionForm(request.POST)
+            if restriction_form.is_valid():
+                restriction_form.save()
 
     context = {
         "goals": goals,
-        "form": goal_form,
+        "goal_form": goal_form,
+        "task_form": task_form,
+        "restriction_form": restriction_form,
     }
     return render(request, "goals/goal_list.html", context)
